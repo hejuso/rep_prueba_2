487ac2dbd123ec77bb29395701d475480290d894
hejuso <hector151293@gmail.com>
2015-01-16 12:49:32 +0100

    Algoritmo de tesorería Versión Inicial

---
+++ Tesoreria/src/Tesoreria.java
@@ -0,0 +1,28 @@
+
+public class Tesoreria {
+
+	public static void main(String[] args) {
+
+		//VARIABLES
+		int gastos [] = {-5,-120,-65};
+		int ingresos [] = {10,3,40,120,60};		
+		int balance=0;
+		//LOGICA
+		
+		for(int i=0;i<ingresos.length;i++)
+		{
+			balance=balance+ingresos[i];
+		}
+		
+		for(int i=0;i<gastos.length;i++)
+		{
+			balance=balance+gastos[i];
+		}
+		//IMPRESION
+		System.out.println("El balance total es: "+balance);
+		
+		
+		
+		}
+
+}
